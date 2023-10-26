7c7a3c9bbe0764f66f435d5479b916a319a7e85b
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2023-10-26 10:49:55 -0300

    making bullets not re enter screen

--- src/entities/bullet.cpp
+++ src/entities/bullet.cpp
@@ -10,13 +10,6 @@ namespace Asteroids {
 		static const float BULLET_RADIUS = 5.0f;
 		static const double BULLET_LIFETIME = 1.0;
 
-		static void updateBulletByScreenCollision(Bullet& bullet) {
-			ScreenUtils::Entity bulletEntity = { bullet.position, Vector2Scale(bullet.direction, bullet.velocity) };
-			ScreenUtils::checkPositionByScreenBounds(bulletEntity);
-
-			bullet.position = bulletEntity.position;
-		}
-
 		Bullet createBullet(Vector2 position, Vector2 direction) {
 			Timer::Timer timer;
 			Timer::startTimer(&timer, BULLET_LIFETIME);
@@ -32,8 +25,6 @@ namespace Asteroids {
 
 		void updateBullet(Bullet& bullet) {
 			bullet.position = Vector2Add(bullet.position, Vector2Scale(bullet.direction, bullet.velocity * GetFrameTime()));
-
-			updateBulletByScreenCollision(bullet);
 
 			if (Timer::timerDone(bullet.lifetimeTimer)) {
 				bullet.shouldRemove = true;
