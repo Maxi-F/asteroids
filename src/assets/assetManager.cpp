31390b0d789a9e3cd0ca4cc528dddd6ebb340422
Maxi-F <maximiliano.a.feldman@gmail.com>
2023-10-29 14:03:01 -0300

    changing assets folder name to res

--- src/assets/assetManager.cpp
+++ src/assets/assetManager.cpp
@@ -12,25 +12,25 @@ namespace Asteroids {
 		static std::vector<AssetWithTexture> textures;
 
 		void init() {
-			AssetWithTexture spaceShip = { Assets::SHIP, LoadTexture("assets/spaceship.png") };
-			AssetWithTexture propulsingSpaceship = { Assets::PROPULSING_SHIP, LoadTexture("assets/propulsingShip.png") };
-			AssetWithTexture explodedShip = { Assets::EXPLODED_SHIP, LoadTexture("assets/explodedShip.png") };
-			AssetWithTexture shieldedShip = { Assets::SHIELDED_SHIP, LoadTexture("assets/shieldedShip.png") };
-			AssetWithTexture shieldedPropulsingShip = { Assets::SHIELDED_PROPULSING_SHIP, LoadTexture("assets/shieldedShip.png") };
+			AssetWithTexture spaceShip = { Assets::SHIP, LoadTexture("res/images/spaceship.png") };
+			AssetWithTexture propulsingSpaceship = { Assets::PROPULSING_SHIP, LoadTexture("res/images/propulsingShip.png") };
+			AssetWithTexture explodedShip = { Assets::EXPLODED_SHIP, LoadTexture("res/images/explodedShip.png") };
+			AssetWithTexture shieldedShip = { Assets::SHIELDED_SHIP, LoadTexture("res/images/shieldedShip.png") };
+			AssetWithTexture shieldedPropulsingShip = { Assets::SHIELDED_PROPULSING_SHIP, LoadTexture("res/images/shieldedShip.png") };
 
-			AssetWithTexture bigAsteroid = { Assets::BIG_ASTEROID, LoadTexture("assets/bigAsteroid.png") };
-			AssetWithTexture mediumAsteroid1 = { Assets::MEDIUM_ASTEROID_1, LoadTexture("assets/mediumAsteroid.png") };
-			AssetWithTexture mediumAsteroid2 = { Assets::MEDIUM_ASTEROID_2, LoadTexture("assets/mediumAsteroid2.png") };
-			AssetWithTexture smallAsteroid = { Assets::SMALL_ASTEROID, LoadTexture("assets/smallAsteroid.png") };
+			AssetWithTexture bigAsteroid = { Assets::BIG_ASTEROID, LoadTexture("res/images/bigAsteroid.png") };
+			AssetWithTexture mediumAsteroid1 = { Assets::MEDIUM_ASTEROID_1, LoadTexture("res/images/mediumAsteroid.png") };
+			AssetWithTexture mediumAsteroid2 = { Assets::MEDIUM_ASTEROID_2, LoadTexture("res/images/mediumAsteroid2.png") };
+			AssetWithTexture smallAsteroid = { Assets::SMALL_ASTEROID, LoadTexture("res/images/smallAsteroid.png") };
 			
-			AssetWithTexture bullet = { Assets::BULLET, LoadTexture("assets/bullet.png") };
-			AssetWithTexture multiBullet = { Assets::MULTI_BULLET, LoadTexture("assets/multiBullet.png") };
+			AssetWithTexture bullet = { Assets::BULLET, LoadTexture("res/images/bullet.png") };
+			AssetWithTexture multiBullet = { Assets::MULTI_BULLET, LoadTexture("res/images/multiBullet.png") };
 			
-			AssetWithTexture bulletsPowerUp = { Assets::BULLETS_POWER_UP, LoadTexture("assets/bulletsPowerup.png") };
-			AssetWithTexture pointsPowerUp = { Assets::POINTS_POWER_UP, LoadTexture("assets/pointsPowerup.png") };
-			AssetWithTexture shieldPowerUp = { Assets::SHIELD_POWER_UP, LoadTexture("assets/shieldPowerup.png") };
+			AssetWithTexture bulletsPowerUp = { Assets::BULLETS_POWER_UP, LoadTexture("res/images/bulletsPowerup.png") };
+			AssetWithTexture pointsPowerUp = { Assets::POINTS_POWER_UP, LoadTexture("res/images/pointsPowerup.png") };
+			AssetWithTexture shieldPowerUp = { Assets::SHIELD_POWER_UP, LoadTexture("res/images/shieldPowerup.png") };
 
-			AssetWithTexture life = { Assets::LIFE, LoadTexture("assets/life.png") };
+			AssetWithTexture life = { Assets::LIFE, LoadTexture("res/images/life.png") };
 
 			textures = { 
 				spaceShip,
